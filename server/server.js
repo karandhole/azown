ca7e5c3fbbd7c165e4254f91a2d62d6a6cd16890
karan dhole <karan.dhole20@pccoepune.org.>
2025-03-22 12:23:55 +0530

    S

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const connectToMongo = require("./db/connect");
+const cors = require("cors");
+connectToMongo();
+const app = express();
+const port = 5000;
+
+app.use(express.json({ limit: "500mb" }));
+app.use(
+  cors()
+);
+
+// Available Routes
+app.use("/user", require("./routes/user"));
+app.use("/property", require("./routes/property"));
+app.use("/admin", require("./routes/admin"));
+app.use("/service", require("./routes/service"));
+app.use("/leadprop", require("./routes/leadprop"));
+app.use("/handler", require("./routes/handler"));
+app.use("/likes", require("./routes/likeprop"));
+
+app.listen(port, () => {
+  console.log(`server is run on ${port}`);
+});
